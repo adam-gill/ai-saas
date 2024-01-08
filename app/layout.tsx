f8c60ad0c069761e4c02b97241a658dac082897d
adam-gill <adam.douglas.gill@gmail.com>
2024-01-08 17:51:05 -0500

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Genius",
   description: "AI Platform",
+  icons: {
+    icon: '/logo.png',
+  }
 };
 
 export default function RootLayout({
